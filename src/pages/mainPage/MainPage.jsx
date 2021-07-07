e85da90a929e0cf70951a4aacef5ec594b2c968c
amirhallaji <a.hallaji.b@gmail.com>
2021-07-07 19:18:50 +0430

    MainPage component added.

---
+++ src/pages/mainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Styles
+import './MainPage.css';
+
+const MainPage = () => {
+      return (
+            <div>
+
+            </div>
+      )
+}
+
+export default MainPage;

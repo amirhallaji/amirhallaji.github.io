29e20acf2e27f22dd25eb69fe1bf33dc997058e6
amirhallaji <a.hallaji.b@gmail.com>
2021-07-08 11:31:23 +0430

    navbar added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { Component } from 'react';
 
+// Styles
+import 'antd/dist/antd.css';
+
 // Components
 import MainPage from './pages/mainPage/MainPage';
 
--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
 
 // Styles
-import './MainPage.css';
+import "./MainPage.css";
+
+// Components
+import { Button } from "antd";
 
 const MainPage = () => {
-      return (
-            <div id='container' style={{minHeight:window.innerHeight}}>
-                  <div id='container-left'>
-                        hi
-                  </div>
-                  <div id='container=right'>
+  return (
+    <div id="container">
+      <div id="navbar">
+        <a className='navbar-item'>Home</a>
+        <a className='navbar-item'>About</a>
+        <a className='navbar-item'>Experience</a>
+        <a className='navbar-item'>Projects</a>
+        <a className='navbar-item'>Skills</a>
+        <a className='navbar-item'>Contact</a>
+        <Button className='navbar-item' type='primary' shape='round'>Download CV</Button>
+      </div>
 
-                  </div>
-
-            </div>
-      )
-}
+      <div id="wrapper">
+        <div id="wrapper-left">fff</div>
+        <div id="wrapper=right">ddd</div>
+      </div>
+    </div>
+  );
+};
 
 export default MainPage;

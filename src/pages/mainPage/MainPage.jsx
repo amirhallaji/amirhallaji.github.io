3cc3faa32765a30e51777e04ed952321dcb1218b
amirhallaji <a.hallaji.b@gmail.com>
2021-07-08 13:09:23 +0430

    font changed; nav-bar enhanced.

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -10,23 +10,27 @@ const MainPage = () => {
   return (
     <div id="container">
       <div id="navbar">
-        <a className='navbar-item'>Home</a>
-        <a className='navbar-item'>About</a>
-        <a className='navbar-item'>Experience</a>
-        <a className='navbar-item'>Projects</a>
-        <a className='navbar-item'>Skills</a>
-        <a className='navbar-item'>Contact</a>
-        <Button className='navbar-item' type='primary' shape='round'>Download CV</Button>
+        <a className='navbar-item' href='#Home'>Home</a>
+        <a className='navbar-item' href='#About'>About</a>
+        <a className='navbar-item' href='#Experience'>Experience</a>
+        <a className='navbar-item' href='#Projects'>Projects</a>
+        <a className='navbar-item' href='#Skills'>Skills</a>
+        <a className='navbar-item' href='#Resume'>Resume</a>
+        <a className='navbar-item' href='#Contact'>Contact</a>
+        {/* <Button className='navbar-item' type='primary' shape='round'>Download CV</Button> */}
       </div>
 
       <div id="wrapper">
-        <div id="wrapper-left">fff</div>
+        <div id="wrapper-left"></div>
         <div id="wrapper-right" style={{minHeight:window.innerHeight}}>
-          <div>
-            home
+          <div className='sections'>
+            <h1>Home</h1>
+            <p>I am a B.SC student studying computer Engineering at Shahid Beheshti University.</p>
+            <p>I love to learn and experience new things in this world.</p>
           </div>
-          <div>
-            about
+          <div className='sections'>
+            <h1>About</h1>
+            
           </div>
         </div>
       </div>

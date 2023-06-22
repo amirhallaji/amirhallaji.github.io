327a019fe99c1d7dc2a2c91f7e30616b7eca9f07
Amir Hallaji <amir.hallaji@snapp.cab>
2023-06-23 01:52:55 +0330

    feat: add projects data

---
+++ src/data.js
@@ -0,0 +1,93 @@
+import beheshti from "./assets/images/behehsti.png";
+import snapp from "./assets/images/snapp.jpeg";
+import salam from "./assets/images/salam.jpeg";
+import mavaratech from "./assets/images/mavaratech.png";
+import tooba from "./assets/images/tooba.jpg";
+import ili from "./assets/images/ili.jpg"
+import snackflix from "./assets/images/snackflix.png";
+import idea from "./assets/images/idea.png";
+import rankings from "./assets/images/rankings.png";
+import beheshtray from "./assets/images/beheshtray.png";
+
+export const educations = [
+  {
+    logos: [beheshti],
+    title:
+      "2022 - 2024, MSc in Artificial Intellignece at Shahid Beheshti University",
+    body: "Most interested in Deep Learning, Machine Learning, Under the supervision of Dr.Hamed Malek",
+  },
+  {
+    logos: [beheshti],
+    title:
+      "2018 - 2022 BSc in Computer Engineering at Shahid Beheshti University",
+    body: "Gratuated as a computer engineer.\nGPA: 18.00 / 20.",
+  },
+];
+
+export const experiences = [
+  {
+    logos: [snapp],
+    title: "2022 - Now, Software Engineer at Snapp Inc",
+    body: "Working as a software engineer at Snapp. It is the first and largest ride-hailing platform in the middle east. (Iranian Uber)",
+  },
+  {
+    logos: [salam],
+    title: "2019, Now, Introduction to programming Teacher",
+    body: "Teaching introduction to programming in C/C++, Java",
+  },
+  {
+    logos: [mavaratech],
+    title: "2020, Backend Developer Intern at Mavaratech",
+    body: "Learned Backend Programming in Java.",
+  },
+  {
+    logos: [tooba],
+    title: "2019, Progrmmer Intern",
+    body: "Learned HTML, CSS",
+  },
+];
+
+export const certifications = [
+  {
+    logos: [ili],
+    linkText: "",
+    link: "",
+    title: "Iran Language Institute",
+    type: "ol",
+    body: [
+      "Studied English from the begining to Advanced Levels",
+    ],
+  },
+  
+];
+
+export const projects = [
+  {
+    logos: [snackflix],
+    linkText: "Click to see the Project",
+    link: "https://github.com/AJob-Recommender/",
+    title: "Linkedin Job Recommender",
+    body: "AJR (Amir Job Recommender) is my bachelor thesis which recommends you a job based on your Skills, Education and Experiences.",
+  },
+  {
+    logos: [idea],
+    linkText: "Click to see the Project",
+    link: "https://github.com/Idea-Thrive/",
+    title: "Idea Thrive",
+    body: "Idea Thrive is a website for managing ideas. In companies, there is a need for a system which manages ideas, gather and sort them for the personnels of that organization.",
+  },
+  {
+    logos: [rankings],
+    linkText: "Click to see the Project",
+    link: "https://github.com/amirhallaji/Computational-Intelligence",
+    title: "Topic News Classification",
+    body: "Topic News classification classifies the Iranina content news to 5 categories. It was the Computational Intelligence final project.",
+  },
+  {
+    logos: [beheshtray],
+    linkText: "Click to see the Project",
+    link: "https://github.com/amirhallaji/Beheshtray-Polling-System",
+    title: "Beheshtray Polling system",
+    body: "Beheshtray is a polling application in which you can submit comments about teachers, upvote and downvote them.",
+  },
+];

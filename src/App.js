4bb15fd6a332bc9ea6cbda952c1b07582e94433c
Amir Hallaji <amir.hallaji@snapp.cab>
2023-06-23 01:39:54 +0330

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import './App.css';
-import { Component } from 'react';
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Section from "./components/Section";
+import SideMenu from "./components/SideMenu";
 
-// Styles
-import 'antd/dist/antd.css';
+function App() {
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
 
-// Components
-import MainPage from './pages/mainPage/MainPage';
+  useEffect(() => {
+    document.body.style.overflow = isSideMenuOpen ? "hidden" : "auto";
+  }, [isSideMenuOpen]);
 
-class App extends Component {
-
-  render() {
-    return (
-      <MainPage />
-    )
-  }
+  return (
+    <>
+      <Header setIsSideMenuOpen={setIsSideMenuOpen} />
+      <SideMenu
+        setIsSideMenuOpen={setIsSideMenuOpen}
+        isSideMenuOpen={isSideMenuOpen}
+      />
+      <main>
+        <Hero />
+        <Section title="education" />
+        <Section title="experiences" />
+        <Section title="certifications" />
+        <Section title="projects" />
+        <Section title="about" />
+        <Section title="contact" />
+      </main>
+      <Footer />
+    </>
+  );
 }
+
 export default App;
